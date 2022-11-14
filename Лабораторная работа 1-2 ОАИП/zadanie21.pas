4c1e5cbac3d065a0260f879772c358156e730dab
BalbesKiryusha43 <118163485+BalbesKiryusha43@users.noreply.github.com>
2022-11-14 10:57:48 +0300

    Add files via upload

---
+++ Лабораторная работа 1-2 ОАИП/zadanie21.pas
@@ -0,0 +1,10 @@
+﻿program KirillP;
+var a,b,c:integer;
+begin
+  writeln('Введите три целых числа');
+  readln(a,b,c);
+  if (a mod 2 = 0) or (b mod 2 = 0) or (c mod 2 = 0 ) then writeln ('Среди трёх целых чисел есть хотя бы одно чётное')
+  else;
+  if (a mod 2 <> 0) or (b mod 2 <> 0) or (c mod 2 <> 0 ) then writeln ('Среди трёх целых чисел есть хотя бы одно нечётное')
+  else;
+end.
---
+++ Лабораторная работа 1-2 ОАИП/zadanie27.pas
@@ -0,0 +1,10 @@
+﻿program KirillP;
+var a,b,c:integer;
+begin
+  writeln('Введите три стороны треугольника');
+  readln(a,b,c);
+  if (a*a+b*b=c*c) then writeln('По теореме Пифагора треугольник прямоугольный')
+  else if (a*a+b*b>c*c) then writeln('По теореме Пифагора треугольник остроугольный')
+  else if (a*a+b*b<c*c) then writeln('По теореме Пифагора треугольник тупоугольный')
+  else;
+end.
